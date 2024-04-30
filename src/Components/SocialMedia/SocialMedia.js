00fe48af14e4f41044c974fe50b1830c3f3fa647
amit yadav <amit-be.y@gyp.in>
2024-04-30 17:27:26 +0530

    first commit

--- src/Components/SocialMedia/SocialMedia.js
+++ src/Components/SocialMedia/SocialMedia.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const SocialMedia = () =>{
     return(
         <>
-            <div className="socialMedai text-orange-500 flex gap-5 items-center justify-center py-6 text-4xl ">
+        <div className="socialMedai text-orange-500 flex gap-5 items-center justify-center py-6 text-4xl ">
        
        <p><Link to="" ><FaFacebookSquare /></Link></p>
        <p><Link to="" ><FaLinkedin /></Link></p>
